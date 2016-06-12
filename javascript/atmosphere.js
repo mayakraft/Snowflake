c06073332bc2ec4eb75566c0806fbcdc8c6bf8b7
robbykraft <robbykraft@gmail.com>
2016-06-12 13:38:16 -0400

    good atmosphere values, atmosphere test

--- javascript/atmosphere.js
+++ javascript/atmosphere.js
@@ -6,61 +6,56 @@
 //             'moisture'}
 
 var Atmosphere = function(length){
-	this.length = length;
+	this.length = int(length);
 	this.pressure = [];
 	this.moisture = [];
 	this.density = [];
 	if(length <= 0){return;}
 
-	var curves = [];
-	for(var waves = 0; waves < 3; waves++){
-		var curve = [];
-		// capture a sine curve with some frequency and offset, relative to this nuber of this.length
-		var offset = random(2*PI)-PI;
-		var frequency = (random(8)+.5) / this.length;
-		if(waves == 1)
-			frequency = (random(2)+4.5) / this.length;
+	var phase1 = random(2*PI)-PI;
+	var phase2 = random(2*PI)-PI;
+	var phase3 = random(2*PI)-PI;
 
-		for(var i = 0; i < this.length; i++){
-			curve.push( cos(offset + PI*frequency * i)*.5 + .5 );  // betwee 0 and 1
-		}
-		curves.push(curve);
+	var freq1 = random(0.5, 10.0) / this.length;
+	var freq2 = random(0.5, 10.0) / this.length;
+	var freq3 = random(4.0, 20.0) / this.length;
+
+	for(var i = 0; i < this.length; i++){		
+		this.pressure[i] = (cos(phase1+freq1*i)*.5+.5);
+		this.moisture[i] = (cos(phase2+freq2*i)*.5+.5);
+		if(cos(phase3+freq3*i) > 0)  this.density[i] = true;
+		else                         this.density[i] = false;
 	}
-	for(var i = 0; i < this.length; i++){
-		this.pressure[i] = (curves[0][i]) * .9 + .05;      //random(1);
-		if(curves[1][i] > 0.5)//random(10) > 5)
-			this.density[i] = true;
-		else 
-			this.density[i] = false;
-		this.moisture[i] = (curves[2][i]) * .9 + .05;    //random(1);
-	}
-	// // manually mess with the data
-	// for(var i = 0; i < 3; i++){
-	// 	var index = int(random(this.length));
-	// 	this.pressure[index] = random(0.0, 0.1);
-	// }
 
 	this.drawGraph = function(rect){
-	// rect is expecting {'x':_, 'y':_, 'width':_, 'height':_, }
-		noStroke();
-		fill(40,255);
+		// rect is expecting {'x':_, 'y':_, 'width':_, 'height':_, }
+		var MARGIN = 10;
+
+		// bounding box
+		stroke(220);
+		fill(240);
 		beginShape();
-		vertex(rect.x, rect.y);
-		vertex(rect.x, rect.y + rect.height);
-		vertex(rect.x + rect.width, rect.y + rect.height);
-		vertex(rect.x + rect.width, rect.y);
+		vertex(rect.x-MARGIN, rect.y-MARGIN);
+		vertex(rect.x-MARGIN, rect.y+MARGIN + rect.height);
+		vertex(rect.x+MARGIN + rect.width, rect.y+MARGIN + rect.height);
+		vertex(rect.x+MARGIN + rect.width, rect.y-MARGIN);
 		endShape(CLOSE);
 
-		for(var i = 0; i < this.length - 1; i++){
-			stroke(255,0,0);
-			line(rect.x + (i)*(rect.width/this.length), rect.y + this.pressure[i] * rect.height, 
-				rect.x + (i+1)*(rect.width/this.length), rect.y + this.pressure[i+1] * rect.height);
-			stroke(0,255,0);
-			line(rect.x + (i)*(rect.width/this.length), rect.y + this.moisture[i] * rect.height, 
-				rect.x + (i+1)*(rect.width/this.length), rect.y + this.moisture[i+1] * rect.height);
-			stroke(70,70,255);
-			line(rect.x + (i)*(rect.width/this.length), rect.y + this.density[i] * rect.height, 
-				rect.x + (i+1)*(rect.width/this.length), rect.y + this.density[i+1] * rect.height);
+		// x=0 origin line
+		stroke(220);
+		line(rect.x-MARGIN, rect.y + .5 * rect.height, rect.x+MARGIN + rect.width, rect.y + .5 * rect.height);
+
+		// pressure, density, moisture
+		for(var i = 0; i < (this.length-1); i++){
+			stroke(255, 128, 128);
+			line(rect.x + (i)/(this.length-1) * rect.width, rect.y + this.pressure[i] * rect.height, 
+			     rect.x + (i+1)/(this.length-1) * rect.width, rect.y + this.pressure[i+1] * rect.height);
+			stroke(128, 255, 128);
+			line(rect.x + (i)/(this.length-1) * rect.width, rect.y + this.moisture[i] * rect.height, 
+			     rect.x + (i+1)/(this.length-1) * rect.width, rect.y + this.moisture[i+1] * rect.height);
+			stroke(128, 128, 255);
+			line(rect.x + (i)/(this.length-1) * rect.width, rect.y + this.density[i] * rect.height, 
+			     rect.x + (i+1)/(this.length-1) * rect.width, rect.y + this.density[i+1] * rect.height);
 		}
 	}
 };
